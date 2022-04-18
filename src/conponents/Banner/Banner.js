9b60ef09841ea1defb1cdd461f78df502afd2249
Tilok Paul <tilokpaul.web@gmail.com>
2022-04-18 23:33:18 +0600

    Update banner content text and ready to submit file

--- src/conponents/Banner/Banner.js
+++ src/conponents/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <section className='md:grid grid-cols-2 container mx-auto'>
             <div className='flex flex-col justify-center px-5 py-20 md:py-5'>
-                <h2 className='text-3xl font-medium decoration-gray-600 uppercase'>Proffesional and Family Dental Care</h2>
+                <h2 className='text-3xl font-medium decoration-gray-600 uppercase'>Dental PRO is Professional and Family Dental Care service</h2>
                 <p className='mt-2'>Are you looking for happy smile teeth dental care services? Yes are in wright place. I providing dental services for your's and your family member. Please check our services and contact if any query. </p>
                 <div>
                     <a href="#contact" className='bg-cyan-500 hover:bg-cyan-800 px-9 py-3 mt-3 mx-auto text-white rounded inline-block'>Contact now</a>
